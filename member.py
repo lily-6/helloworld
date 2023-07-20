7719f9008c6c55999ae39ca15063de9175dea480
Ririko Yamaguchi <130433022+lily-6@users.noreply.github.com>
2023-07-20 11:47:09 +0900

    add myself
    変更

--- member.py
+++ member.py
@@ -24,11 +24,9 @@ mlist.append(newmember)
 newmember = Member("房州優樹", "よろしくです！")
 mlist.append(newmember)
 
-### 以下に自分を追加する ###
 newmember = Member("吉田 羅生", "よろしくです！")
 mlist.append(newmember)
 
-### 以下に自分を追加する ###
 newmember = Member("渡邉　雄太", "よろしく！")
 mlist.append(newmember)
 
